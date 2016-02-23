7c96f98f142d10844419996852f260427be70ab0
Valeriy Mars <valeriy.mars@gmail.com>
2016-02-23 20:24:53 +0200

    Using existing backbone boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var entry = './src/app/main.js',
+  output = {
+    path: __dirname,
+    filename: 'main.js'
+  };
+
+module.exports.development = {
+    debug : true,
+    devtool : 'eval',
+    entry: entry,
+    output: output,
+    module : {
+        loaders : [
+            { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' },
+            { test: /\.hbs$/, loader: 'handlebars-loader' }
+        ]
+    }
+};
+
+module.exports.production = {
+    debug: false,
+    entry: entry,
+    output: output,
+    module : {
+        loaders : [
+            { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' },
+            { test: /\.hbs$/, loader: 'handlebars-loader' }
+        ]
+    }
+};
